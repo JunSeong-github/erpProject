24baef0537d1ee6a81f4b35bf56a739330257461
JunSeong <JunSeong-github>
2026-06-11 18:15:52 +0900

    20260611 -> 로그인 기능 구현 완료(관리자, 직원) -> 승인 반려기능 관리자만 가능하게 수정

--- backEnd/frontEnd/src/lib/axios.ts
+++ backEnd/frontEnd/src/lib/axios.ts
@@ -1,4 +1,3 @@
 import axios from "axios";
 // export const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE?? "https://erpproject-pu8e.onrender.com",withCredentials: false, });
 export const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE?? "http://localhost:8080",withCredentials: true, });
-단
